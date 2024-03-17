b65a8cd418e7736d118f88064d5ac7a6e537d1af
webdev-mostofa <mdmostofaa24@gmail.com>
2024-03-18 05:43:32 +0600

    done

--- src/components/Carts/Carts.jsx
+++ src/components/Carts/Carts.jsx
@@ -1,16 +1,26 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const Carts = ({ carts, handleBtn }) => {
   const { image, title, description, price } = carts;
+  const [isClick, setIsClick] = useState(false);
+  const clickHandle = () => {
+    setIsClick(!isClick);
+    
+  };
+  console.log(isClick);
+  const handleMainBtn = ()=>{
+    handleBtn(carts);
+    clickHandle();
+  }
   return (
-    <div className="space-y-4 shadow-2xl p-5 rounded-md">
+    <div className={`space-y-4 shadow-2xl p-5 rounded-md ${isClick? 'bg-red-500':''}`}>
       <img className="w-[200px] mx-auto" src={image} alt="" />
       <h2>{title.slice(0, 10)}</h2>
       <h2>{description.slice(0, 100)}</h2>
       <div className="flex items-center gap-3">
         <p>Price: {price}</p>
-        <button
-          onClick={() => handleBtn(carts)}
+        <button onClick={()=> handleMainBtn()}
           className="px-3 py-1 bg-lime-500 text-white rounded-md"
         >
           Purchase
